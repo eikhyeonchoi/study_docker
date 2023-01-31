7ddf453906235a0d733b7c00c81e8a7aef2187f6
ikhyunchoi <eikhyeon.choi@gmail.com>
2023-01-31 21:21:50 +0900

    db 설정 Dockfile로 전환 및 db 테이블 초기화 구성

---
+++ test/dockertest02/db/init/init.d
@@ -0,0 +1,15 @@
+CREATE USER 'dbuser'@'localhost' IDENTIFIED BY 'dbuser';
+CREATE USER 'dbuser'@'%' IDENTIFIED BY 'dbuser';
+
+GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'%';
+
+CREATE TABLE `member` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
