337a8b720b90de0df5d6bbc6ae70219e7bcfb0e5
ikhyunchoi <eikhyeon.choi@gmail.com>
2023-01-31 22:01:14 +0900

    Update init.sql

--- test/dockertest02/db/conf/init.sql
+++ test/dockertest02/db/conf/init.sql
@@ -7,6 +7,9 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'%';
 
+CREATE DATABASE IF NOT EXISTS testdb;
+use testdb;
+
 CREATE TABLE `member` (
 	`id` INT(10) NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
