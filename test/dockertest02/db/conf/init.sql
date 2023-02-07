1ceea2ec1c58bbe1b0fe55bcfcac15ecde4f397c
ikhyunchoi <eikhyeon.choi@gmail.com>
2023-02-07 21:27:30 +0900

    .

--- test/dockertest02/db/conf/init.sql
+++ test/dockertest02/db/conf/init.sql
@@ -7,15 +7,17 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'%';
 
-CREATE DATABASE IF NOT EXISTS testdb;
-use testdb;
+CREATE DATABASE IF NOT EXISTS foobar;
+use foobar;
 
-CREATE TABLE `member` (
+CREATE TABLE `user` (
 	`id` INT(10) NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`created_dt` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb4_0900_ai_ci'
 ENGINE=InnoDB
 ;
+
+INSERT INTO `foobar`.`user` (`name`) VALUES ('111');
